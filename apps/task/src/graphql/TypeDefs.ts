212a058e15ef20c32349331347c475775058c2ce
AbedDoesCode <abdelrahman.ahmed605@hotmail.com>
2024-06-23 13:04:11 -0400

    Created task microservice (#5)
    * Installed required packages
    
    * Created graphql type definitions
    
    * Defined the graphql context interface which contains data on authenticated users
    
    * Created function to authenticate tokens of users in aws cognito pool
    
    * Created query resolver functions
    
    * Created mutation resolver functions
    
    * Created resolver object with mutation and queries and AWSDateTime scalar to be used to input dates in dynamoDB
    
    * Created server file to start apollo express server with defined type defintions and resolvers and user authentication to allow access to resolvers

---
+++ apps/task/src/graphql/TypeDefs.ts
@@ -0,0 +1,110 @@
+import { gql } from "apollo-server-express";
+
+export const typeDefs = gql`
+  scalar AWSDateTime
+
+  type Task {
+    PK: String!
+    SK: String!
+    Title: String!
+    Description: String
+    Status: String!
+    CreatedAt: AWSDateTime!
+    TaskUpdatedAt: AWSDateTime
+    TaskCompletedAt: AWSDateTime
+    DueDate_NotificationSent: String!
+    NotificationSent: Boolean!
+    DueDate: AWSDateTime!
+  }
+
+  type GraphQLError {
+    key: String!
+    error: String!
+  }
+
+  type MutationResponse {
+    success: Boolean!
+    Task: Task
+    errors: [GraphQLError]
+  }
+
+  type DeleteTaskResponse {
+    success: Boolean!
+    errors: [GraphQLError]
+  }
+
+  input CreateTaskInput {
+    Title: String!
+    Description: String
+    DueDate: AWSDateTime!
+  }
+
+  input UpdateTaskInput {
+    TaskId: String!
+    Title: String
+    Description: String
+    Status: String
+    DueDate: AWSDateTime
+  }
+
+  type Query {
+    tasks: [Task]
+    task(taskId: String!): Task
+  }
+
+  type Mutation {
+    createTask(input: CreateTaskInput!): MutationResponse
+    updateTask(input: UpdateTaskInput!): MutationResponse
+    deleteTask(taskId: String!): MutationResponse
+  }
+`;
+
+export interface GraphQLError {
+  key: string;
+  error: string;
+}
+
+export interface Task {
+  PK: string;
+  SK: string;
+  Title: string;
+  Description?: string | null;
+  Status: string;
+  CreatedAt: string;
+  TaskUpdatedAt?: string | null;
+  TaskCompletedAt?: string | null;
+  DueDate_NotificationSent: string;
+  NotificationSent: boolean;
+  DueDate: string;
+}
+
+export interface CreateTaskInput {
+  Title: string;
+  Description?: string | null;
+  DueDate: Date;
+}
+
+export interface UpdateTaskInput {
+  TaskId: string;
+  Title?: string;
+  Description?: string;
+  Status?: string;
+  DueDate?: Date;
+  TaskCompletedAt?: string;
+}
+
+export interface DeleteTaskInput {
+    taskId: string;
+  }
+
+export interface MutationResponse {
+  success: boolean;
+  Task?: Task | null;
+  errors?: GraphQLError[];
+}
+
+export enum Status {
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+}
