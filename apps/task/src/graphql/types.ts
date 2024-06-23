fc43b420530bd31d6f70bd9266945746b44602d1
AbdelrahmanAhmed605 <abdelrahman.ahmed605@hotmail.com>
2024-06-23 12:59:17 -0400

    Defined the graphql context interface which contains data on authenticated users

---
+++ apps/task/src/graphql/types.ts
@@ -0,0 +1,16 @@
+export interface ContextType {
+  user: {
+    sub: string;
+    iss: string;
+    client_id: string;
+    origin_jti: string;
+    event_id: string;
+    token_use: string;
+    scope: string;
+    auth_time: number;
+    exp: number;
+    iat: number;
+    jti: string;
+    username: string;
+  };
+}
