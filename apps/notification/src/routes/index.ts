aaa022617ccbefb4cc921429816daf4829bd5891
AbdelrahmanAhmed605 <abdelrahman.ahmed605@hotmail.com>
2024-06-27 19:47:01 -0400

    Added health check route to be used by load balancer

--- apps/notification/src/routes/index.ts
+++ apps/notification/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userApiRoutes from "./user_api";
 import authMiddleware from "../auth/authMiddleware";
 import lambdaApiRoutes from "./lambda_api"
+import healthRoutes from "./service_health";
 
 const router = Router();
 
 router.use("/user", authMiddleware, userApiRoutes);
 router.use("/lambda", lambdaApiRoutes);
+router.use("/health", healthRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
